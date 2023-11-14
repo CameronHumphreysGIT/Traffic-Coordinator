23da54ebc99e73f5e4d1854578972a8596f31574
CameronHumphreysGIT <Cam117version@gmail.com>
2023-11-14 17:01:52 -0500

    Implemented Catch 2 for testing with a sample test

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/Traffic-Coordinator")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Traffic-Coordinator")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -35,6 +35,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("C:/Users/Cameron/Documents/COMP4905/Traffic Coordinator/build/src/cmake_install.cmake")
+  include("C:/Users/Cameron/Documents/COMP4905/Traffic Coordinator/build/test/cmake_install.cmake")
 
 endif()
 
---
+++ build/test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Cameron/Documents/COMP4905/Traffic Coordinator/test
+# Build directory: C:/Users/Cameron/Documents/COMP4905/Traffic Coordinator/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/Users/Cameron/Documents/COMP4905/Traffic Coordinator/build/test/tests-b12d07c_include.cmake")
